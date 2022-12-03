1ea4f6eeaee4849d655e6f61e806d5796411bb6a
makar <107802459+GreenShrek@users.noreply.github.com>
2022-12-03 22:43:31 +0200

    changes

--- Makar/task1.js
+++ Makar/task1.js
@@ -1,14 +1,21 @@
 const prompt = require('prompt-sync')();
 let base = []
-let user = {}
-let command = 0;
+function makeUser(name,surname,age,gender,index) {
+    return {
+        name:name,
+        surname:surname,
+        age:age,
+        gender:gender,
+        index:index,
+    }
+}
 while(true) {
 
-    command = +prompt("1:Создать юзера 2:Обновить юзера 3:Удалить юзера 4:Найти юзера 6:Вывести все данные ")
+    command = +prompt("1:Создать юзера 2:Обновить юзера 3:Удалить юзера 4:Найти юзера 5:Отсортировать всех юзеров 6:Вывести все данные ")
 
     switch (command){
         case 1:
-
+            
             createUser();
         break;
         case 2:
@@ -31,55 +38,26 @@ while(true) {
     if(value > 6) {
         console.log('Такой команды не существует')
     }
-    if(value == ' ' || value <= 0) break;
+    if(value <= 0) break;
 }   
-
-
-function createUser () {
-    let userName = prompt('Введите ник ')
-    let userSurname = prompt('Введите фамилию ')
-    let userGender = prompt('Ваш гендер male/female')
-    let userAge = prompt('Введите возраст ')
-    user = {
-        name: userName,
-        surname: userSurname,
-        gender: userGender,
-        age: userAge
-    }
-    base.push(`${base.length + 1}` , user)
-    console.log(user)
+function createUser() {
+    let userName = prompt('имя: ')
+    let userSurname = prompt('фамилия: ')
+    let userAge = +prompt('возраст: ')
+    let userGender = prompt('гендер: ')
+    let userIndex = base.length;
+    let user = makeUser(userName,userSurname,userAge,userGender,userIndex)
+    base.push(user)
 }
-
-function resetUser () {
+function resetUser() {
     console.log(base)
-    let key = prompt('Введите номер юзера которого хотите изменить: ')
-    let change = prompt('Что вы хотите изменить?(name/surname/gender/age) ')
-    let newValue= prompt('Введите изминения ')
-    switch(change) {
-        case 'name':
-        user.name = newValue
-        break;
-        case 'surname':
-        user.surname = newValue
-        break;
-        case 'gender':
-        user.gender = newValue
-        break;
-        case 'age':
-        user.age = newValue
-        break;
+    let criterion = +prompt()
+    for(let i = 0;i < base.length;i++) {
+        console.log(i)
+        if(i == criterion) {
+            let value = prompt()
+            base[i] = value 
+        }
+        
     }
-    // console.log(base)
 }
-function deleteUser() {
-    let i = +prompt('Введите айди юзера которого хотите удалить: ')
-    let remove = base.splice(i,1)
-    console.log(base)
-}
-// function searchUser() {
-//     let info = prompt('Введите дaнныe юзера: ')
-//     for(info in user) {
-//         console.log(user)
-//         break;
-//     }
-// }
