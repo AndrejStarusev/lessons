4a85eebdc7f699b806fe90b399b1a0da37e5b56b
alexvorn <voroncov.sasha93@gmail.com>
2022-12-05 18:18:13 +0200

    add update, delete users

--- alexander/alexTask1.js
+++ alexander/alexTask1.js
@@ -1,5 +1,8 @@
 const prompt = require('prompt-sync')();
 let command;
+let update;
+let indexUser;
+const users = [];
 
 function CreateUser() {
     return {    
@@ -10,15 +13,50 @@ function CreateUser() {
     };
 }
 
-const users = [];
+function updateUsers(index) {
+    while (update !== false) {
+        update = prompt('введите значение которое хотите изменить, 0 закончить операцию ');
+        const user = users[index];
+        let value = prompt('введите значение ');
+        switch(update) {
+            case 'name' : 
+                user.name = value;
+            break;
+            case 'surname' :
+                user.surname = value;
+            break;
+            case 'age' :
+                user.age = value;
+            break;
+            case 'gender' :
+                user.gender = value;
+            break;
+    
+            default : update = false;
+        }
+    }
+
+}
+
+
 
 while (command !== false) {
-    command = prompt('Выберите операцию от 0 до 6 ');
+    command = prompt('Выберите операцию от 1 до 6, 0 закончить операции ');
     switch(command) {
         case '1' : 
             const user = CreateUser();
             console.log(user);
             users.push(user);
+        break;
+        case '2' :
+            indexUser = prompt('Введите индекс юзера ');
+            updateUsers(indexUser)
+            console.log(users)
+        break;
+        case '3' :
+            indexUser = prompt('Введите индекс юзера ');
+            users.splice(indexUser, 1)
+            console.log(users);
         break;
 
         default : command = false;
