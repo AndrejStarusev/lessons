c61f74af06555744b19d3e68db057a15a8838f4b
alexvorn <voroncov.sasha93@gmail.com>
2022-12-07 14:17:02 +0200

    add sortUsers, users list

--- alexander/alexTask1.js
+++ alexander/alexTask1.js
@@ -4,6 +4,7 @@ let update;
 let indexUser;
 let specificProperty;
 let arrFindUsers;
+let sort;
 const breakKey = '0';
 
 const users = [];
@@ -54,10 +55,31 @@ function searchUsers(value) {
     return arrFindUsers
 }
 
+function sortAscUsers (sort, ascOrDesc) {
+    console.log(ascOrDesc)
+    if (ascOrDesc === '1') {
+        for (let i = 0; i < users.length - 1; i++) {
+            if ( users[i][sort] > users[i + 1][sort]) {
+                const buff = users[i];
+                users[i] = users[i + 1];
+                users[i + 1] = buff;
+            }
+        }
+    }else{
+        for (let i = 0; i < users.length - 1; i++) {
+            if ( users[i][sort] < users[i + 1][sort]) {
+                const buff = users[i];
+                users[i] = users[i + 1];
+                users[i + 1] = buff;
+            }
+        }
+
+    }
+}
 
 
 while (command !== false) {
-    command = prompt('Выберите операцию от 1 до 6, 0 закончить операции ');
+    command = prompt('Выберите операцию: 1)Создать юзера, 2)Редактировать, 3)Удалить, 4)Поиск, 5)Сортировка, 6)Вывести всех юзеров, 0)закончить ');
     switch(command) {
         case '1' : 
             const user = CreateUser();
@@ -75,6 +97,15 @@ while (command !== false) {
             specificProperty = prompt('Введите выбор поиска юзера(name, surname, age, gender) ');
             searchUsers(specificProperty);
             console.log(arrFindUsers);
+        break;
+        case '5' :
+            sort = prompt('Введите значения сартировки(name, surname, age, gender) ');
+            let ascOrDesc = prompt('выберите варианты сартировки: 1)возрастание, 2) убывание ');
+            sortAscUsers(sort, ascOrDesc);
+            console.log(users)
+        break;
+        case '6' :
+            console.log(users)
         break;
 
         default : command = false;
