9a1cba5ca3123ef48311975b3f12aa56d59cfe35
alexvorn <voroncov.sasha93@gmail.com>
2022-12-12 14:39:16 +0200

    add function quicksort

--- alexander/alexTask2.js
+++ alexander/alexTask2.js
@@ -1,16 +1,43 @@
 const prompt = require('prompt-sync')();
-const broken = '0';
-const arr = [];
+const arr = [1, 10, 14, 5, 6, 12, 3, 2, 8];
 
 
-function sort () {
-    while(value !== false) {
-        let value = prompt('введите значения для массива ');
-        if(value === broken) {
-            arr.push(value)
+// function sort () {
+//     while(value !== false) {
+//         let value = prompt('введите значения для массива ');
+//         if(value === broken) {
+//             arr.push(value)
+//         }
+//     }
+//     let sortAscDesc = prompt('выберите вариант сортировки : 1)возрастание, 2)убывание ')
+// }
+
+// sort()
+
+function quicksort(array) {
+    const index = Math.floor(Math.random() * array.length);
+    const currentItem = array[index];
+    const more = [];
+    const less = [];
+
+    if(array.length < 2) {
+        return array
+    }
+
+    for (let i = 0; i < array.length; i++) {
+        if (i === index) {
+            continue
+        }
+
+        if (array[i] > currentItem) {
+            more.push(array[i]);
+        } else {
+            less.push(array[i]);
         }
     }
-    let sortAscDesc = prompt('выберите вариант сортировки : 1)возрастание, 2)убывание ')
+
+    return [...quicksort(less), currentItem, ...quicksort(more)]
+
 }
 
-sort()
+console.log(quicksort(arr));
