7ad26851afaa6817a651664187172963830d4d7e
max-baykalov <baykalov.maxim@gmail.com>
2023-01-12 19:23:18 +0100

    add params

--- max/task3.js
+++ max/task3.js
@@ -8,13 +8,17 @@ app.get('/', function(req, res) {
   res.send('hello world');
 });
 
-app.post('/calc', function(req, res) {
+app.post('/calc/:operator', function(req, res) {
+  const operator = req.params.operator;
   const { a, b } = req.body;
 
-  const result = a + b; 
+  console.log(operator);
+  const result = a + b;
+
 
   res.send({
-    result
+    result,
+    operator,
   });
 });
 
