07b0d3c78748ea35965609c3a754cbf829e75440
makar <107802459+GreenShrek@users.noreply.github.com>
2023-01-13 18:39:13 +0200

    done +-

--- Makar/task2.js
+++ Makar/task2.js
@@ -1,6 +1,6 @@
 const prompt = require('prompt-sync')();
 
-const arr = [1,2,6,5,4,8,10,3,22,21,100,99,101]
+const arr = [6,9,1,4,2,3,7,5,8,10]
 
 function sort1 (arr,order) {
     for(let j = 0;j < arr.length; j++) {
@@ -14,7 +14,7 @@ function sort1 (arr,order) {
     }
     return arr;
 }
-console.log(sort1(arr,'>'))
+// console.log(sort1(arr,'<'))
 
 
 
@@ -27,7 +27,8 @@ function sort2(arr,order) {
                 arr[j] = arr[j - 1];
                 j--;
             }
-        } else {
+        } 
+        else {
             while (j > 0 && arr[j - 1] < current) {
                 arr[j] = arr[j - 1];
                 j--;
@@ -39,14 +40,63 @@ function sort2(arr,order) {
     }
     return arr;
 };
-console.log(sort2(arr,'>'))
-function checkMethod(sort1,sort2) {
-    const arrASC = arr.sort();
-    const arrDESC = arr.reverse();
-    if(sort1,sort2(arr) == arrASC || sort1,sort2(arr,'<' == arrDESC)) {
-        return true;
-    } else{
-        return false;
+// console.log(sort2(arr,'>'))
+
+function sort3(arr,order) {
+    for (let i = 0, l = arr.length, k = l - 1; i < k; i++) {
+        let indexMin = i;
+        for (let j = i + 1; j < l; j++) {
+            if (order == '>' ? arr[indexMin] > arr[j] : arr[indexMin] < arr[j]) {
+                indexMin = j;
+            }
+        }
+        if (indexMin !== i) {
+            [arr[i], arr[indexMin]] = [arr[indexMin], arr[i]];
+        }
     }
+    return arr;
 }
-console.log(checkMethod(sort1,sort2))
+// console.log(sort3(arr,'>'))
+
+
+
+
+
+function testAsc(sort) {
+    const arrASC = [1,2,3,4,5,6,7,8,9,10]
+    const ascRes = sort;
+    // console.log(arrASC)
+    console.log(ascRes)
+    for(let i = 0;i < arr.length;i++) {
+        if(ascRes[i] !== arrASC[i]) {
+            console.log('sort1 doesn`t work')
+            return false;
+        }
+    }
+    return true;
+}
+console.log(testAsc(sort1(arr,'>')))
+
+function testAsc(sort) {
+    const arrDESC = [10,9,8,7,6,5,4,3,2,1]
+    const descRes = sort;
+    console.log(arrDESC)
+    console.log(descRes)
+    for(let i = 0;i < arr.length;i++) {
+        if(descRes[i] !== arrDESC[i]) {
+            console.log('sort1 doesn`t work')
+            return false;
+        }
+    }
+    return true;
+}
+console.log(testAsc(sort1(arr,'<')))
+
+
+// function time(sort) {
+// console.time(`${sort}`)
+// sort(arr)
+// console.timeEnd(`${sort}`)
+// return sort;
+// }
+// console.log(time(sort1(arr)))
