b96333000e1d936c2a283ac25aab18947f0c5935
Radion Volovoi <Radikvolovoy@gmail.com>
2023-01-15 15:53:22 +0200

    added http calculator

---
+++ Radion/RadionTask3.js
@@ -0,0 +1,52 @@
+const express = require("express");
+
+function calculate(a, b, operator) {
+  switch (operator) {
+    case "plus":
+      return a + b;
+      break;
+    case "minus":
+      return a - b;
+      break;
+    case "multiply":
+      return a * b;
+      break;
+    case "division":
+      return a / b;
+      break;
+    case "degree":
+      return a ** b;
+      break;
+    case "remainder":
+      return a % b;
+      break;
+    default:
+      return false;
+      break;
+  }
+}
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
+
+app.post("/calc/:operator", function (req, res) {
+  const operator = req.params.operator;
+  console.log(1, req.body);
+  const { a, b } = req.body;
+
+  const result = calculate(a, b, operator);
+
+  res.send({
+    result,
+    operator,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("App is started");
+});
