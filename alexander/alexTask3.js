4817903abfd5f502c1dcc319d04e4ab6688f12c1
alexvorn <voroncov.sasha93@gmail.com>
2023-01-15 15:54:43 +0200

    add func calc

---
+++ alexander/alexTask3.js
@@ -0,0 +1,43 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+function calc(a, b, operator) {
+  switch (operator) {
+    case "plus":
+      return a + b
+    case "minus":
+      return a - b
+    case "multiply":
+      return a * b
+    case "divide":
+      return a / b
+    default: throw new Error('operator not found')
+  }
+}
+app.get('/', function(req, res) {
+  res.send('zalupa');
+});
+
+app.post('/calc/:operator', function(req, res) {
+  const operator = req.params.operator;
+  const { a, b } = req.body;
+  let result;
+  
+  try {
+    result = calc(a, b, operator);
+  } catch(err) {
+    result = err.message;
+  }
+  
+  res.send({
+    result,
+    operator,
+  });
+});
+
+app.listen(3000, () => {
+  console.log('App is started');
+})
