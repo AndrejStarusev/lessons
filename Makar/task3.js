8db8c93bb79cfc6123b336d04a840383039bab4c
Makar <makarlrom1@gmail.com>
2023-01-16 19:17:01 +0200

    add calculator 2

--- Makar/task3.js
+++ Makar/task3.js
@@ -8,14 +8,31 @@ app.get('/', function(req, res) {
   res.send('hello world');
 });
 
-app.post('/calc', function(req, res) {
-  const { a, b } = req.body;
+app.post('/calc/:command', function(req, res) {
+  const { a, b} = req.body;
+  const command = req.params.command;
+  let result = 0;
+  switch(command) {
+    case "minus":
+      result = a - b;
+    break;
+    case "plus":
+      result = a + b;
+    break;
+    case "multi":
+      result = a * b;
+    break;
+    case "divide":
+      result = a / b;
+    break;
+  }
 
-  const result = a + b;
+ 
 
 
   res.send({
     result,
+    command
   });
 });
 app.listen(3000, () => {
