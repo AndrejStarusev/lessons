bf5f864e70361ee6e05b37cd51076a57ca2175fd
Makar <makarlrom1@gmail.com>
2023-01-16 17:59:10 +0200

    calculator done

--- Makar/task3.js
+++ Makar/task3.js
@@ -8,17 +8,14 @@ app.get('/', function(req, res) {
   res.send('hello world');
 });
 
-app.post('/calc/:operator', function(req, res) {
-  const operator = req.params.operator;
+app.post('/calc', function(req, res) {
   const { a, b } = req.body;
 
-  console.log(operator);
   const result = a + b;
 
 
   res.send({
     result,
-    operator,
   });
 });
 app.listen(3000, () => {
