88beb849535d154e47c8bc625ab35fdb3cae5c4c
Кирилл Трачук <>
2023-01-29 12:10:30 +0200

    add cors

--- Kirill/task3.js
+++ Kirill/task3.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', function(req, res) {
   res.send('helloo world');
