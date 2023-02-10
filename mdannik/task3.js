91ea082048622af16e5206bccccb1c19a6840808
kali <kali@localhost.localdomain>
2023-02-09 18:56:45 -0600

    add cors

--- mdannik/task3.js
+++ mdannik/task3.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+const cors = require('cors')
 const app = express();
 
 app.use(express.json());
-
+app.use(cors())
 app.get('/', function(req, res) {
   res.send('hello world');
 });
